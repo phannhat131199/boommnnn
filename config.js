4ddf6f867c4d27bb845a950170c3bf61b96a6b81
phannhat131199 <89177767+phannhat131199@users.noreply.github.com>
2021-08-19 11:25:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hé lô chào em nhá chúc em buổi sáng-trưa-chìu-tối vui vẻ tốt lành hahahah',
+    introDesc: 'Sau gần 1 tháng nc với nhau thì nếu em hong ngại thì trả lời anh câu này nha <3',
+    btnIntro: 'Bấm vào đây lẹ lẹ =))))) ',
+    title: 'Có lúc nào em cảm thấy rất nhớ với thíchhhhh anh hokkkkkkk',
+    desc: 'Hong trả lời mà out ra là tới công chiện đó nhaaaa ',
+    btnYes: 'Nhớ với thích nhắm luônnnn dấu bé baaa',
+    btnNo: 'Mé nghe sến quá tha em iii anh N :)))))) ',
+    question:'Sao chọn câu đó dạ 🤣🤣 ',
+    btnReply: 'Send it to me',
+    reply: 'Be cau se anh N là chúa tể đẹp trai, trúa hề, hoàng tử dth nên em lúc nào cũng nhớ với thích anh hết á nha hahahahahah',
+    mess: 'Anh biết mờ^^ Ngại quóooo',
+    messDesc: 'Nhắc lân en nờ là đừng thức khuya nữa nha nghe hong với đừng lâu lâu suy nghĩ rùi bùn nữa, có bùn thì nói anh nè để anh lau nc mắt cho chứ hong gì :)))))) Hi vọng qua dịch sẽ đc gặp em ha, tới lúc đó đừng biến mất á. Anh sợ v lắm hihi :>>>>',
+    btnAccept: 'Dạ vâng em biết ùiiii <3',
+    messLink: 'https://www.facebook.com/phanminhnhat1311' 
+}
